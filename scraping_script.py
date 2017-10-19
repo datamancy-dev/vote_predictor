c8896fa6ea37a78620d6c904e68dc9625b9ec767
brocas <frlimaa@gmail.com>
2017-10-18 23:11:42 -0700

    Shifted gears into a more complete sraper, the data it returns will be easier to work with, though I did lose a day...

--- scraping_script.py
+++ scraping_script.py
@@ -2,14 +2,28 @@ import requests
 import pandas as pd
 import pickle
 import requests
+from bs4 import BeautifulSoup
+import json
 
-def payload(keyfile):
-    """Imports key from file returns GET request payload"""
-    key = ''
-    with open(keyfile, 'r') as f:
-        for line in f:
-            key = line
+def get_votes(congress, year, limit=0):
+    """Given the congress number and year pickle each
+    bill's roll call data in pandas dataframe format"""
 
-    return {'X_API_Key':key}
+    url = "https://www.govtrack.us/data/congress/"+congress+"/votes/"+year+"/"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    titles = soup.select("a")
+    titles = [title.text for title in titles]
+    #Strip the root directory
+    titles = titles[1:]
+    urls = [url+title+"/data.json" for title in titles]
+    
+    #I know it is inefficient to place this here but idk if I can
+    #exit any of the of the for loops in list comprehensions early ASK or LU
+    #limit the size for debugging purposes
+    if limit != 0 and limit < len(urls):
+        urls = urls[:limit]
 
-
+    for uri in urls:
+        vreq = request(uri)
+        rollcall = json.loads(r.text)
