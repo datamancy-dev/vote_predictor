6939f2abab43cdb4a3201b2961e9fd3d219c50c5
brocas <frlimaa@gmail.com>
2017-10-18 19:30:49 -0700

    Oops... actual beginning of scraping_script

--- scraping_script.py
+++ scraping_script.py
@@ -1,3 +1,14 @@
 import requests
-import pandas
+import pandas as pd
 import pickle
+import requests
+
+script, keyfile = argv
+key = ''
+with open(keyfile, 'r') as f:
+    for line in f:
+        key = line
+
+PAYLOAD = {'X_API_Key':key}
+
+
