7b69750222a5761c60898dff8b7ee96c9d7b8790
brocas <frlimaa@gmail.com>
2017-10-18 19:48:09 -0700

    Updated scraping script making previous two commits pointless. Ignore.

--- scraping_script.py
+++ scraping_script.py
@@ -3,12 +3,13 @@ import pandas as pd
 import pickle
 import requests
 
-script, keyfile = argv
-key = ''
-with open(keyfile, 'r') as f:
-    for line in f:
-        key = line
+def payload(keyfile):
+    """Imports key from file returns GET request payload"""
+    key = ''
+    with open(keyfile, 'r') as f:
+        for line in f:
+            key = line
 
-PAYLOAD = {'X_API_Key':key}
+    return {'X_API_Key':key}
 
 
