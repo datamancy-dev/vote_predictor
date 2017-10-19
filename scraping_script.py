2cb6bd697aab9a9651323a04ac8ba3c5a188b2e9
brocas <frlimaa@gmail.com>
2017-10-19 08:22:52 -0700

    Begun building the voting matrix directly from scraped data

--- scraping_script.py
+++ scraping_script.py
@@ -5,25 +5,30 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-def get_votes(congress, year, limit=0):
-    """Given the congress number and year pickle each
-    bill's roll call data in pandas dataframe format"""
 
-    url = "https://www.govtrack.us/data/congress/"+congress+"/votes/"+year+"/"
+def get_urls(congress, year):
+    """Computes the url path"""
+    url = "https://www.govtrack.us/data/congress/"+str(congress)+"/votes/"+str(year)+"/"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
     titles = soup.select("a")
     titles = [title.text for title in titles]
     #Strip the root directory
     titles = titles[1:]
-    urls = [url+title+"/data.json" for title in titles]
-    
-    #I know it is inefficient to place this here but idk if I can
-    #exit any of the of the for loops in list comprehensions early ASK or LU
+    return [url+title+"data.json" for title in titles]
+
+def get_votes(congress, year, limit=0):
+    """Given the congress number and year pickle each
+    bill's roll call data in pandas dataframe format"""
+    urls = get_urls(congress, year)
     #limit the size for debugging purposes
     if limit != 0 and limit < len(urls):
         urls = urls[:limit]
 
     for uri in urls:
-        vreq = request(uri)
-        rollcall = json.loads(r.text)
+        print(uri)
+        #vreq = request(uri)
+        #rollcall = json.loads(r.text)
+        
+
+
