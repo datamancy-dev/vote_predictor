3271a24110c62585596529759c48aff3d55bc2f9
brocas <frlimaa@gmail.com>
2017-10-19 17:16:20 -0700

    Added column formatter (?)

--- scraping_script.py
+++ scraping_script.py
@@ -5,9 +5,22 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
+def get_votes(congress, year, limit=0):
+    """Given the congress number and year yield a pandas series to be 
+    aggregated in a pandas df"""
+
+    urls = get_urls(congress, year)
+    #limit the size for debugging purposes
+    if limit > 0 and limit < len(urls):
+        urls = urls[:limit]
+
+    for uri in urls:
+        print(uri)
+        vreq = request(uri)
+        yield j_to_vseries(json.loads(r.text))
 
 def get_urls(congress, year):
-    """Computes the url path"""
+    """Computes the url paths for the given congress and year"""
     url = "https://www.govtrack.us/data/congress/"+str(congress)+"/votes/"+str(year)+"/"
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "html.parser")
@@ -17,18 +30,12 @@ def get_urls(congress, year):
     titles = titles[1:]
     return [url+title+"data.json" for title in titles]
 
-def get_votes(congress, year, limit=0):
-    """Given the congress number and year pickle each
-    bill's roll call data in pandas dataframe format"""
-    urls = get_urls(congress, year)
-    #limit the size for debugging purposes
-    if limit != 0 and limit < len(urls):
-        urls = urls[:limit]
 
-    for uri in urls:
-        print(uri)
-        #vreq = request(uri)
-        #rollcall = json.loads(r.text)
-        
-
-
+def j_to_vseries(js):
+    """Turns a json roll call page into a Pandas series"""
+    dic = {}
+    for votetype in js["votes"]:
+        for vote in js["votes"][votetype]:
+            dic[vote["id"]] = votetype
+    
+    return pd.Series(dic, name=js["vote_id"])
