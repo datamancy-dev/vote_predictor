4a1d26176a2c09a73fe3a9dec3d868b0bda38f6c
brocas <frlimaa@gmail.com>
2017-11-15 10:51:00 -0800

    Described last steps, updated README to indicate this is still a WIP

--- make_topics_df.py
+++ make_topics_df.py
@@ -7,6 +7,8 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 
+NAME, NUM_TOPICS, LDA_MODEL = argv
+
 #Just placeholder so that cvect can exist
 class StemTokenizer(object):
     def __init__():
@@ -14,3 +16,17 @@ class StemTokenizer(object):
 
 
 
+
+
+def extract_y_and_house(inds):
+    years = []
+    house = []
+    for ind in inds:
+        years.append(int(inds[:4]))
+        house.append(inds[4])
+
+    return years, house
+
+if __name__ == '__main__':
+    y_and_h = extract_y_and_house()
+    
