74f54f60e09e7c6443fde6ceb21ae8694ac1d1d6
brocas <frlimaa@gmail.com>
2017-11-16 17:02:28 -0800

    Finished the vote pipeline

--- make_vote_df.py
+++ make_vote_df.py
@@ -1,9 +1,12 @@
 import re
 import pickle
+from sys import argv
 from collections import defaultdict
 import subprocess as sp
 import resource_paths as rp
 import pandas as pd
+
+NAME, CHAMBER, OUTPUT=argv
 
 def separate_by_congress(votefp):
     congresses = [str(num) for num in xrange(109,116)]
@@ -17,5 +20,52 @@ def separate_by_congress(votefp):
 
     return links_list
 
+
+def create_congress_dflst(linkslist, house_initial):
+    dfs = []
+    for lst in linkslist:
+        cur_cong_votes =[]
+        for link in linkslist[lst]:
+            with open(link, "rb") as f:
+                df = pickle.load(f)
+            df = reload_without_dupes(df,link)
+            df = append_year(df, link, house_initial)
+            cur_cong_votes.append(df)
+        if len(cur_cong_votes) > 1:
+            cur_cong_votes[1] = cur_cong_votes[1].iloc[:,4:]
+        dfs.append(pd.concat(cur_cong_votes, axis=1))
+
+    return dfs
+
+def append_year(df, link, house_initial):
+    find_year = re.compile(r"[0-9]{4}")
+    new_name = find_year.search(link).group(0) + house_initial + "-"
+    df = df.add_prefix(new_name)
+
+    return df
+
+
+def reload_without_dupes(df, fp):
+    """Pandas bundles duplicates in a dataframe...
+       This behavior (and the existance of duplicates)
+       was unexpected so to avoid scraping the data again.
+       This is the workaround"""
+    fp = fp[:-4] + ".csv"
+    df.to_csv(fp, encoding='utf-8', sep=',')
+    df = pd.read_csv(fp, encoding='utf-8', index_col=0, mangle_dupe_cols=True, sep=',')
+
+    sp.call(["rm", fp])
+
+    return df
+
+
 if __name__ == '__main__':
-    
+    votefp = rp.get_all_dfs(CHAMBER+"vote")
+
+    linkslist = separate_by_congress(votefp)
+
+    dfs = create_congress_dflst(linkslist, CHAMBER)
+
+    with open("./"+OUTPUT+".pkl", "wb") as f:
+        pickle.dump(dfs, f)
+
