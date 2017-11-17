c3ef7fdd8e3a29eb2af3086a473be3f41f079728
brocas <frlimaa@gmail.com>
2017-11-17 15:09:37 -0800

    Finished the vote pipeline along with the individual congressmen list

--- make_vote_df.py
+++ make_vote_df.py
@@ -1,4 +1,4 @@
-"""This file makes the vote dataframes as well as 
+"""This file makes the vote dataframes as well as
 the unique lawmakers dictionary """
 import re
 import pickle
@@ -8,7 +8,7 @@ import subprocess as sp
 import resource_paths as rp
 import pandas as pd
 
-NAME, CHAMBER, OUTPUT=argv
+NAME, CHAMBER=argv
 
 def separate_by_congress(votefp):
     congresses = [str(num) for num in xrange(109,116)]
@@ -23,31 +23,44 @@ def separate_by_congress(votefp):
     return links_list
 
 
+def year_to_cong(year):
+    ytc = {2005:109, 2006:109, 2008:110, 2009:111, 2010:111, 2011:112,
+           2012:112, 2014:113, 2015:114, 2016:114, 2017:115}
+
+    return ytc[year]
+
+
+
 def create_congress_dflst(linkslist, house_initial):
     dfs = []
     for lst in linkslist:
         cur_cong_votes =[]
+        years=[]
         for link in linkslist[lst]:
             with open(link, "rb") as f:
                 df = pickle.load(f)
             df = reload_without_dupes(df,link)
-            df = append_year(df, link, house_initial)
+            df, year= append_year(df, link, house_initial)
             cur_cong_votes.append(df)
+            years.append(year)
+
         if len(cur_cong_votes) > 1:
             cur_cong_votes[1] = cur_cong_votes[1].iloc[:,4:]
-        dfs.append(pd.concat(cur_cong_votes, axis=1))
+
+        final_df = pd.concat(cur_cong_votes, axis=1)
+        final_df["congress"]=year_to_cong(years[0])
+
+        dfs.append(final_df)
 
     return dfs
 
 def append_year(df, link, house_initial):
     find_year = re.compile(r"[0-9]{4}")
-    year = find_year.search(link).group(-1)
+    year = find_year.search(link).group(0)
     new_name = year + house_initial + "-"
     df = df.add_prefix(new_name)
-    #This is kinda yanky but the fastest solution I found
-    df["year"] = int(year)
 
-    return df
+    return df, int(year)
 
 
 def reload_without_dupes(df, fp):
@@ -70,7 +83,10 @@ def unique_lawmakers(votes):
 
     for df in votes:
         for lawmaker in df.index.values.tolist():
-            unique_l[lawmaker].add(df["year"])
+            congress = int(df["congress"].iloc[0])
+            unique_l[lawmaker].add(congress)
+
+
     return unique_l
 
 
@@ -81,6 +97,11 @@ if __name__ == '__main__':
 
     dfs = create_congress_dflst(linkslist, CHAMBER)
 
-    with open("./"+OUTPUT+".pkl", "wb") as f:
+    unique = unique_lawmakers(dfs)
+
+    with open("./"+CHAMBER+"votes_p_congress.pkl", "wb") as f:
         pickle.dump(dfs, f)
+
+    with open("./"+CHAMBER+"_lawmakers.pkl", "wb") as f:
+        pickle.dump(unique, f)
 
