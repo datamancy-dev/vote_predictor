a7bbcd04880af742a52130d074b9f5dcc8cc8664
brocas <frlimaa@gmail.com>
2017-11-21 09:29:01 -0800

    Made the skeleton of my final model;s code

--- make_models.py
+++ make_models.py
@@ -3,22 +3,63 @@ from sys import argv
 import pandas as pd
 import resource_path as rp
 
-def make_lawmakers_df(lawm_dict, vote_resource):
-    lawm = load_lawmakers(lawm_dict)
-    congtoind = {109:2, 110:4, 111:3, 112:6, 113:5, 114:1, 115:0}
+def make_lawmakers_df(lawm_path, vote_resource, chamber):
+    """Make the lawmaker model"""
+    lawm = load_resource(lawm_path)
+    lawmaker_model_list = []
 
     votes_p_congress = load_resource(vote_resource)
-    lawm_list = []
+    response_cols = build_response_cols(votes_p_congress)
 
-            
+    dataframes = build_variables(response_cols)
+
+    series_response_cols = to_series(response_cols)
+
+    pack_resource(dataframes, "./"+chamber+"_modeldfs.pkl")
+    pack_resource(series_response_cols, "./"+chamber+"_responsecols.pkl")
 
 
-def build_response_col():
-    """Given a lawmaker, return the response variable column
-       for its model"""
+def pack_resource(resource, filepath):
+    """Pickles given resource in filepath"""
+    with open(filepath, "wb") as f:
+        pickle.dump(resource, filepath)
+
+def to_series(response_cols):
+    """Given a list of dictionaries return a list of series containing the same
+    elements as the dictionaries"""
+
+
+def build_variables(response_cols):
+    """Given list of dictionaries build a dataframe per dictionary that
+    contains only the laws specified by the dictionary keys. Returns a list of
+    dataframes of length equals to the number of lawmakers"""
+
+
+def build_response_cols(votes_p_congress):
+    """Builds all of the lawmakers response columns"""
+    response_col = {}
+    congtoind = {109:2, 110:4, 111:3, 112:6, 113:5, 114:1, 115:0}
+
     for lawm, sessions in lawm_dict.items():
         for session in sessions:
-            #get lawmakers row and save it as a series
+            response_col = merge_dicts(response_col,
+                    congress_response_col(votes_p_congress[congtoind[session]],
+                        lawm))
+
+    return response_col
+
+
+def congress_response_col(vote_df, lawm):
+    """Given a vote dataframe and a lawmaker extract the votes from
+       that lawmaker
+
+       Returns a dictionary with bills in its keys and votes as its
+       items"""
+
+
+
+
+
 
 
 def load_resource(file_path):
